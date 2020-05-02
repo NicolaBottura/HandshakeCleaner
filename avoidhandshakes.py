070d1900e77169da1ca44a5e2a62491d842c9333
Nicola Bottura <root@arpwatch-clients.acme-14.test>
2020-05-02 09:42:01 +0000

    Final commit(hopefully)

--- avoidhandshakes.py
+++ avoidhandshakes.py
@@ -13,74 +13,83 @@ welcome = """
                               __
           ___..__       r----[__]
   __..--""  ._ __.'     *   ,'  '.
-              "-..__       : ---- :       
+              "-..__       : ---- :
             '"--..__";     | SOAP |
  ___        '--...__"";    : ---- :
     `-..__ '"---..._;"     |  NB  |
-          ' ""----'         "----"    
+          ' ""----'         "----"
 
 "Always wash your hands, even after a DHCP handshake - Nicola Bottura 2020"
-
 
 @Nicola Bottura,
 @Giuseppe D'Agostino,
 @Giorgia Lombardi.
 """
-		
+
 count = 0
 time = []
 macs = []
 dict = {}
 
+
 print(welcome)
 
+
 def handle_dhcp(packet):
-	global count, time, macs, dict
+    global count, time, macs, dict
 
-	newtime = (str(datetime.datetime.now()).split(" ")[1])
-	newmac = packet.src
+    newtime = (str(datetime.datetime.now()).split(" ")[1])
+    newmac = packet.src
 
-	if packet[DHCP].options[0][1] == 1: #DHCP DISCOVER PACKET
-		count += 1
-		for time, mac in dict.items():
-			if mac == newmac and count > 1:
-				hand_washer(time, newtime, newmac)
+    if packet[DHCP].options[0][1] == 1:  # DHCP DISCOVER PACKET
+        count += 1
+        for time, mac in dict.items():
+            if mac == newmac and count > 1:
+                val = hand_washer(time, newtime, newmac)
+                if val == 0:
+                    sleep(60)
+                    break
 
-	dict[newtime] = newmac
+    dict[newtime] = newmac
+
 
 def start():
-	sniff(filter="port 67 and port 68", prn=handle_dhcp, store=0, iface='eth0')
+    sniff(filter="udp and (port 67 or port 68)", prn=handle_dhcp, store=0)
+
 
 def hand_washer(time, newtime, newmac):
-	hour1 = time.split(":")[0]
-	hour2 = newtime.split(":")[0]
-	min1 = time.split(":")[1]
-	min2 = newtime.split(":")[1]
+    hour1 = time.split(":")[0]
+    hour2 = newtime.split(":")[0]
+    min1 = time.split(":")[1]
+    min2 = newtime.split(":")[1]
 
-	# If the time is the same I don't need to check the milliseconds
-	# If the hour is the same but not the minutes and there are in range of 10 mins send the frame
-	if (time == newtime) or ((hour1 == hour2) and (int(min2) - int(min1) in range(10))):
-		send_frame(time, newtime, newmac)
+    # If the time is the same I don't need to check the milliseconds
+    # If the hour is the same but not the minutes and there are in range of 10 mins send the frame
+    if (time == newtime) or ((hour1 == hour2) and (int(min2) - int(min1) in range(10))):
+        send_frame(time, newtime, newmac)
 
-		print("***********************************")
-		print("** My job here is done...exiting **\n")
-		print("** ...but you didn't do anything **\n")
-		print("***********************************")
+        return 0
+    
+    else:
+        return 1
 
-		sys.exit()
 
 def send_frame(time, newtime, newmac):
-	ether = Ether()
-	ether.type = 0x0101
-	ether.dst = "66:a3:2e:83:1e:7f" # Internal Router MAC
-	pkt = ether/Raw(load = "Warning: detected possible DHCP flooding attack by " + newmac + " at" + time + "and then again at " + newtime)
+    ether = Ether()
+    ether.type = 0x0101
+    ether.dst = "66:a3:2e:83:1e:7f"  # Internal Router MAC
+    pkt = ether/Raw(load="Warning: detected possible DHCP flooding attack by " +
+                    newmac + " at" + time + "and then again at " + newtime)
 
-	print("\nWARNING: Possible DHCP flooding attack detected!!!\n")
-	print("...sending an alert to the router.\n")
-	sendp(pkt, verbose=0)
+    print("\nWARNING: Possible DHCP flooding attack detected!!!\n")
+    print("...sending an alert to the router.\n")
+    sendp(pkt, verbose=0)
+
 
 try:
-	start()
+    while(True):
+        start()
 
 except KeyboardInterrupt:
-	sys.exit()
+    print("\nExiting")
+    sys.exit()
